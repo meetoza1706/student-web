15edfd4cb92d9c6208798ed45068e38231a743a7
Meet Oza <ozamee17@gmail.com>
2024-06-16 13:56:33 +0530

    bug fixes 16th june

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request , session,  jsonify, redirect, url_for
+from flask import Flask, render_template, request, session, jsonify, redirect, url_for
 from flask_mysqldb import MySQL
-import datetime
+from datetime import datetime, timedelta
 import random
 import os
 import smtplib
@@ -25,93 +25,102 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'studentweb'
 
 mysql = MySQL(app)
-bcrypt = Bcrypt()
+bcrypt = Bcrypt(app)
 
+# Schedule dictionary
 schedule = {
-    "Monday": {"time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
-            "lectures": ["SE", "ICT", "ADBMS-LAB", "ADBMS-LAB", "Break", "DS-LAB", "DS-LAB", "end of day"],
-            "class_numbers": ["Class 1", "Class 2", "Class 3", "Class 4", "", "Class 5", "Class 6", ""]},
-    "Tuesday": {"time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:20 PM", "02:10 PM"],
-                "lectures": ["MS", "WT", "ICT", "DS", "Break", "ADBMS", "SE", "end of day"],
-                "class_numbers": ["Class 7", "Class 8", "Class 9", "Class 10", "", "Class 11", "Class 12", ""]},
-    "Wednesday": {"time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
-                "lectures": ["ADBMS-LAB", "ADBMS-LAB", "ADBMS", "DS", "Break", "EVA", "EVA", "end of day"],
-                "class_numbers": ["Class 13", "Class 14", "Class 15", "Class 16", "", "Class 17", "Class 18", ""]},
-    "Thursday": {"time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
-                "lectures": ["SE-LAB", "SE-LAB", "SE", "ICT", "Break", "WT-LAB", "WT-LAB", "end of day"],
-                "class_numbers": ["Class 19", "Class 20", "Class 21", "Class 22", "", "Class 23", "Class 24", ""]},
-    "Friday": {"time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
-            "lectures": ["MS", "ADBMS", "WT-LAB", "WT-LAB", "Break", "EVA", "EVA", "end of day"],
-            "class_numbers": ["Class 25", "Class 26", "Class 27", "Class 28", "", "Class 29", "Class 30", ""]},
-    "Saturday": {"time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM"],
-                "lectures": ["MS", "ADBMS", "WT-LAB", "WT-LAB", "end of day"],
-                "class_numbers": ["Class 31", "Class 32", "Class 33", "Class 34", ""]}
+    "Monday": {
+        "time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
+        "lectures": ["SE", "ICT", "ADBMS-LAB", "ADBMS-LAB", "Break", "DS-LAB", "DS-LAB", "end of day"],
+        "class_numbers": ["Class 1", "Class 2", "Class 3", "Class 4", "", "Class 5", "Class 6", ""]
+    },
+    "Tuesday": {
+        "time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:20 PM", "02:10 PM"],
+        "lectures": ["MS", "WT", "ICT", "DS", "Break", "ADBMS", "SE", "end of day"],
+        "class_numbers": ["Class 7", "Class 8", "Class 9", "Class 10", "", "Class 11", "Class 12", ""]
+    },
+    "Wednesday": {
+        "time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
+        "lectures": ["ADBMS-LAB", "ADBMS-LAB", "ADBMS", "DS", "Break", "EVA", "EVA", "end of day"],
+        "class_numbers": ["Class 13", "Class 14", "Class 15", "Class 16", "", "Class 17", "Class 18", ""]
+    },
+    "Thursday": {
+        "time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
+        "lectures": ["SE-LAB", "SE-LAB", "SE", "ICT", "Break", "WT-LAB", "WT-LAB", "end of day"],
+        "class_numbers": ["Class 19", "Class 20", "Class 21", "Class 22", "", "Class 23", "Class 24", ""]
+    },
+    "Friday": {
+        "time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM", "12:30 PM", "01:30 PM", "02:10 PM"],
+        "lectures": ["MS", "ADBMS", "WT-LAB", "WT-LAB", "Break", "EVA", "EVA", "end of day"],
+        "class_numbers": ["Class 25", "Class 26", "Class 27", "Class 28", "", "Class 29", "Class 30", ""]
+    },
+    "Saturday": {
+        "time_slots": ["08:30 AM", "09:20 AM", "10:10 AM", "11:00 AM", "11:50 AM"],
+        "lectures": ["MS", "ADBMS", "WT-LAB", "WT-LAB", "end of day"],
+        "class_numbers": ["Class 31", "Class 32", "Class 33", "Class 34", ""]
+    }
 }
+
+def get_current_lecture_and_class(schedule):
+    now = datetime.now()
+    current_day = now.strftime("%A")
+    current_time = now.time()
+
+    if current_day in schedule:
+        day_schedule = schedule[current_day]
+        for i, time_slot in enumerate(day_schedule["time_slots"]):
+            start_time = datetime.strptime(time_slot, "%I:%M %p").time()
+            if i < len(day_schedule["time_slots"]) - 1:
+                end_time = datetime.strptime(day_schedule["time_slots"][i + 1], "%I:%M %p").time()
+            else:
+                end_time = (datetime.strptime(time_slot, "%I:%M %p") + timedelta(hours=1)).time()
+
+            if start_time <= current_time < end_time:
+                current_lecture = day_schedule["lectures"][i]
+                current_class = day_schedule["class_numbers"][i]
+                if i < len(day_schedule["time_slots"]) - 1:
+                    timing = f"{time_slot} to {day_schedule['time_slots'][i + 1]}"
+                else:
+                    timing = f"{time_slot} to end of day"
+                break
+        else:
+            current_lecture = "No lectures going on Right now 😁"
+            current_class = "no classes just relax!!"
+            timing = "Enjoy your off time :)"
+    else:
+        current_lecture = "Enjoy the day off mate 😁"
+        current_class = "    "
+        timing = ""
+
+    return current_lecture, current_class, timing
+
 
 @app.route('/')
 def home():
-    current_time = datetime.datetime.now()
-    
-    weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    weekday_number = current_time.weekday()
-    weekday_name = weekdays[weekday_number]
+    current_lecture, current_class, timing = get_current_lecture_and_class(schedule)
 
-    schedule_info = schedule.get(weekday_name)
-    current_lecture = ""
-    current_class = ""
-    timing = ""
-
-    if schedule_info:
-        if weekday_name == "Saturday":
-            if current_time.hour < 11 or (current_time.hour == 11 and current_time.minute <= 50):
-                for i, time_slot in enumerate(schedule_info["time_slots"]):
-                    slot_time = datetime.datetime.strptime(time_slot, "%I:%M %p")
-                    slot_time = slot_time.replace(year=current_time.year, month=current_time.month, day=current_time.day)
-                    if current_time < slot_time:
-                        current_lecture = schedule_info["lectures"][i]
-                        current_class = schedule_info["class_numbers"][i]
-                        if i == len(schedule_info["time_slots"]) - 1:
-                            timing = f"{schedule_info['time_slots'][i]} - End of day"
-                        else:
-                            timing = f"{schedule_info['time_slots'][i]} - {schedule_info['time_slots'][i+1]}"
-                        break
-            else:
-                current_lecture = "No lectures going on Right now 😁"
-                current_class = "no classes just relax!!"
-                timing = "Enjoy your off time :)"
-        else:
-            for i, time_slot in enumerate(schedule_info["time_slots"]):
-                slot_time = datetime.datetime.strptime(time_slot, "%I:%M %p")
-                slot_time = slot_time.replace(year=current_time.year, month=current_time.month, day=current_time.day)
-                if current_time < slot_time:
-                    current_lecture = schedule_info["lectures"][i-1]
-                    current_class = schedule_info["class_numbers"][i-1]
-                    if i == len(schedule_info["time_slots"]) - 1:
-                        timing = f"{schedule_info['time_slots'][i-1]} - End of day"
-                    else:
-                        timing = f"{schedule_info['time_slots'][i-1]} - {schedule_info['time_slots'][i]}"
-                    if current_lecture == "end of day":
-                        current_lecture = ""
-                    break
-            else:
-                current_lecture = schedule_info["lectures"][-1]
-                current_class = schedule_info["class_numbers"][-1]
-                timing = f"{schedule_info['time_slots'][-1]} - End of day"
-                if current_lecture == "end of day":
-                        current_lecture = ""
-
-    if 5 <= current_time.hour < 12:
+    now = datetime.now()
+    if 5 <= now.hour < 12:
         greeting = "Good Morning!"
-    elif 12 <= current_time.hour < 18:
+    elif 12 <= now.hour < 18:
         greeting = "Good Afternoon!"
-    elif 18 <= current_time.hour < 22:
+    elif 18 <= now.hour < 22:
         greeting = "Good Evening!"
     else:
         greeting = "Good Evening!"
 
-    username = session.get('username', 'guest')  # Get username from session or default to 'guest'
-    
-    return render_template('index.html', current_lecture=current_lecture, current_class=current_class, timing=timing, greeting=greeting, username=username)
+    username = session.get('username', 'guest')
+    ausername = session.get('username')
+    first_letter = username[0]
+    if 'logged_in' in session:
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT email FROM user_data WHERE username = %s', (username,))
+        email = cursor.fetchone()[0]
+    else:
+        email = None
+
+    return render_template('index.html', current_lecture=current_lecture, current_class=current_class, timing=timing, greeting=greeting, username=username, ausername=ausername, email=email)
+
 def hash_password(password):
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     return hashed_password
@@ -222,8 +231,8 @@ def login():
         account = cursor.fetchone()
         
         if account:
-            username_from_db = account[0]  # Access username using integer index 0
-            hashed_password = account[1]  # Access password using integer index 1
+            username_from_db = account[0]
+            hashed_password = account[1]  
             
             if bcrypt.check_password_hash(hashed_password, password):
                 session['logged_in'] = True
@@ -238,7 +247,6 @@ def login():
 
     return render_template('login.html', msg=msg)
 
-
 @app.route('/dashboard')
 def dashboard():
     if 'logged_in' in session:
@@ -246,7 +254,7 @@ def dashboard():
         return f'Welcome, {username}! This is your dashboard.'
     else:
         return redirect(url_for('login'))
-    
+
 @app.route('/forgot', methods=['GET', 'POST'])
 def forgot_password():
     msg = ''
@@ -296,6 +304,15 @@ def reset_password():
                 msg = 'Error resetting password: ' + str(e)
     return render_template('reset_password.html', msg=msg)
 
-    
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('logged_in', None)
+    return redirect(url_for('home'))
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
